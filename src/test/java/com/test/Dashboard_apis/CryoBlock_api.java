3c350492a9366e90063c9284a97373270ae5ffe4
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-04-06 12:20:26 +0530

    CryoBlock_api.java

--- src/test/java/com/test/Dashboard_apis/CryoBlock_api.java
+++ src/test/java/com/test/Dashboard_apis/CryoBlock_api.java
@@ -19,4 +19,68 @@ public class CryoBlock_api {
         System.out.println("API request to the CryoBlock passed. Status code: " + statusCode1);
 	}
 
+@Test(priority=2)
+    public void api_CryoBlocking_Waiting()
+           {
+           Response response1 = RestAssured
+                   .given()
+                   .auth()
+                   .basic("admin", "admin")
+                   .when()
+                   .get("https://apollo2.humanbrain.in/dashboard/queryCryoBlock/1");
+
+           int statusCode1 = response1.getStatusCode();
+           if (statusCode1 == 200) {
+           	System.out.println("_________________________******_______________________");
+               System.out.println("API request to the Dashboard Cryo_Blocking_Waiting passed. Status code: " + statusCode1);
+               System.out.println("_________________________******_______________________");
+           } else {
+           	System.out.println("_________________________******_______________________");
+               System.out.println("API request to the Dashboard Cryo_Blocking_Waiting failed. Status code: " + statusCode1);
+           }
+           Assert.assertEquals(statusCode1, 200, "API request to the Dashboard API request to the Dashboard Cryo_Blocking_Waiting failed");
+           
+       }
+	@Test(priority=3)
+	public void api_Frozen_InProgress() {
+		 Response response1 = RestAssured
+                 .given()
+                 .auth()
+                 .basic("admin", "admin")
+                 .when()
+                 .get("https://apollo2.humanbrain.in/dashboard/queryCryoBlock/2");
+
+         int statusCode2 = response1.getStatusCode();
+         if (statusCode2 == 200) {
+         	System.out.println("_________________________******_______________________");
+             System.out.println("API request to the Dashboard Cryo_Blocking_Frozen_InProgress Passed");
+             System.out.println("_________________________******_______________________");
+         } else {
+         	System.out.println("_________________________******_______________________");
+             System.out.println("API request to the Dashboard Cryo_Blocking_Frozen_InProgress failed. Status code: " + statusCode2);
+         }
+         Assert.assertEquals(statusCode2, 200, "API request to the Dashboard API request to the Dashboard Cryo_Blocking_Frozen_InProgress");
+         
+     }
+	@Test(priority=4)
+	public void api_CryoBlocking_Out() {
+		 Response response1 = RestAssured
+                .given()
+                .auth()
+                .basic("admin", "admin")
+                .when()
+                .get("https://apollo2.humanbrain.in/dashboard/queryCryoBlock/3");
+
+        int statusCode2 = response1.getStatusCode();
+        if (statusCode2 == 200) {
+        	System.out.println("_________________________******_______________________");
+            System.out.println("API request to the Dashboard Cryo_Blocking_Out passed. Status code: " + statusCode2);
+            System.out.println("_________________________******_______________________");
+        } else {
+        	System.out.println("_________________________******_______________________");
+            System.out.println("API request to the Dashboard Cryo_Blocking_Out  failed. Status code: " + statusCode2);
+        }
+        Assert.assertEquals(statusCode2, 200, "API request to the Dashboard Cryo_Blocking_Out failed");
+        
+    }
 }
